529ee746809fd2adb385988ea1b8b576238044ca
Aziz ul Hassan <aziz.hassan@nxb.com.pk>
2023-08-31 15:47:31 +0500

    added relationships to the models

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Country extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name', 'code'];
+
+    public function states() {
+        return $this->hasMany(State::class);
+    }
+
+    public function employees() {
+        return $this->hasMany(Employee::class);
+    }
 }
--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -8,4 +8,33 @@ use Illuminate\Database\Eloquent\Model;
 class Employee extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'country_id',
+        'state_id',
+        'city_id',
+        'department_id',
+        'first_name',
+        'last_name',
+        'address',
+        'zip_code',
+        'birth_date',
+        'hired_date'
+    ];
+
+    public function department() {
+        return $this->belongsTo(Department::class);
+    }
+
+    public function country() {
+        return $this->belongsTo(Country::class);
+    }
+
+    public function state() {
+        return $this->belongsTo(State::class);
+    }
+
+    public function city() {
+        return $this->belongsTo(City::class);
+    }
 }
